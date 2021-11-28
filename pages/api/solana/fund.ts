1bb0eaebad1153b0683f6d85feb8d28deccfe415
o0oi1i <o0oi1i@icloud.com>
2021-11-28 14:47:48 +0800

    Fund the Solana account with SOL

--- pages/api/solana/fund.ts
+++ pages/api/solana/fund.ts
@@ -1,6 +1,7 @@
 import {Connection, PublicKey, LAMPORTS_PER_SOL} from '@solana/web3.js';
 import type {NextApiRequest, NextApiResponse} from 'next';
 import {getNodeURL} from '@figment-solana/lib';
+import {Keypair} from '@solana/web3.js';
 
 export default async function fund(
   req: NextApiRequest,
@@ -10,9 +11,9 @@ export default async function fund(
     const {network, address} = req.body;
     const url = getNodeURL(network);
     const connection = new Connection(url, 'confirmed');
-    const publicKey = undefined;
-    const hash = undefined;
-    await undefined;
+    const publicKey = new PublicKey(address);
+    const hash = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
+    await connection.confirmTransaction(hash);
     res.status(200).json(hash);
   } catch (error) {
     let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
